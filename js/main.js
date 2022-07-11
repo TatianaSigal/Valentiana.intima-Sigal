37ecae3258d2962e6b8df6507a02907f3b817f6e
TatianaSigal <tatiana.sigal@bue.edu.ar>
2022-07-11 11:13:07 -0300

    agregando funciones al carrito

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/* Desafío Simulador interactivo*/
+let totalCarrito = 0
+
+/*function agregarAlCarrito(producto, stock) {
+    const tenemosStock = validarStock(stock);
+    if(tenemosStock == 'Tenemos Stock'){
+        console.log('Agregaste Producto al carrito:' + producto);
+        totalCarrito++;
+    }
+    else{
+        console.log('No hay más stock de este modelo');
+    }
+    }
+   
+ 
+function validarStock(stockDelProducto) {
+    if(stockDelProducto > 0){
+        return 'Tenemos Stock';     
+    }
+    else{ return 'No quedó Stock';
+}
+}
+
+agregarAlCarrito('Conjunto Cindirella',2)
+/*Lo nombré acá porque cuando agregué stock no me funciona tocando el botón, con el producto sólo si agregaba bien*/
+
+function agregarAlCarrito(producto, ) {
+  
+        console.log('Agregaste Producto al carrito:' + producto);
+        totalCarrito++;
+    
+    }
+    /*Si lo pongo así el boton me va agregando los productos, puse"coma" y la cantidad para hacer con stock desde el html pero no me funciona*/
